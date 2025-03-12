b313a8cefd5ad32e4a9d630abb2f04d257c74063
codetiger <codetiger806@gmail.com>
2025-03-12 14:27:06 +0800

    fix: use scripts

--- ddns-go/files/ddns-go.sh
+++ ddns-go/files/ddns-go.sh
@@ -7,12 +7,6 @@ conf="$homeDir/config.yaml"
 params="-c $conf -l :$2"
 
 case "$1" in
-    clear_log_cron)
-        LOG_SIZE=$(ls -l $logFile | awk '{print int($5/1024)}')
-        if [ $LOG_SIZE -gt 100 ]; then
-           echo $(tail -n 100 $logFile ) > $logFile
-        fi
-    ;;
     start)
         /usr/bin/$program $params >> $logFile 2>&1
     ;;
--- nezha/files/nezha.sh
+++ nezha/files/nezha.sh
@@ -7,12 +7,6 @@ conf="$homeDir/agent.yaml"
 params="-c $conf"
 
 case "$1" in
-    clear_log_cron)
-        LOG_SIZE=$(ls -l $logFile | awk '{print int($5/1024)}')
-        if [ $LOG_SIZE -gt 100 ]; then
-           echo $(tail -n 100 $logFile ) > $logFile
-        fi
-    ;;
     start)
         /usr/bin/${program}-agent $params >> $logFile 2>&1
     ;;
